b73b247cb5a70718ea083d66e1a5ad2c3268e37e
Tóth Tamás László <114387336+Maker-Tamas@users.noreply.github.com>
2026-02-10 17:48:30 +0100

    Add date specific validation for reserve parking spot use case.

--- ParkingManagementSystem.Application/ReserveParkingSpot/ReserveParkingSpotCommandValidator.cs
+++ ParkingManagementSystem.Application/ReserveParkingSpot/ReserveParkingSpotCommandValidator.cs
@@ -12,6 +12,10 @@ public class ReserveParkingSpotCommandValidator : AbstractValidator<ReserveParki
         RuleFor(x => x.Dates)
             .NotEmpty().WithMessage("At least one date is required.")
             .Must(dates => dates.All(d => d >= DateOnly.FromDateTime(DateTime.Now)))
-            .WithMessage("Dates cannot be in the past.");
+            .WithMessage("Dates cannot be in the past.")
+            .Must(dates => dates.Distinct().Count() == dates.Count)
+            .WithMessage("Dates must be unique.")
+            .Must(dates => dates.All(d => d <= DateOnly.FromDateTime(DateTime.Now.AddDays(14))))
+            .WithMessage("Dates cannot be more than 2 weeks in the future.");
     }
 }
