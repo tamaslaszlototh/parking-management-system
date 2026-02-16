361583ebdd53a1f2a94a7c8735752de1286db11a
Tóth Tamás László <114387336+Maker-Tamas@users.noreply.github.com>
2026-02-16 17:03:16 +0100

    Add web app project.

---
+++ ParkingManagementSystem.WebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ParkingManagementSystem.WebApp.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
---
+++ ParkingManagementSystem.WebApp/Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+app.MapRazorPages()
+    .WithStaticAssets();
+
+app.Run();
